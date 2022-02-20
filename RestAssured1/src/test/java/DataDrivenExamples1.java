729b61c40985813abb8c41887fdb869010eeca0c
Bartosz Wieczorek <bartosz.wieczorek1993@gmail.com>
2022-02-20 16:22:44 +0100

    RestAssured basics

--- RestAssured1/src/test/java/DataDrivenExamples1.java
+++ RestAssured1/src/test/java/DataDrivenExamples1.java
@@ -1,34 +1,14 @@
 import io.restassured.http.ContentType;
 import org.json.simple.JSONObject;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 
-public class DataDrivenExamples1 {
+public class DataDrivenExamples1 extends DataForTests{
 
-    @DataProvider(name = "DataForPost")
-    public Object[][] dataForPost(){
-        /*Object[][] data = new Object[2][3];
 
-        data[0][0] = "Albert";
-        data[0][1] = "Einstein";
-        data[0][2] = 1;
-        data[1][0] = "Thomas";
-        data[1][1] = "Shelby";
-        data[1][2] = 2;
-
-        return data;*/
-
-        return new Object[][] {
-                {
-                    "Graham","Bell",1
-                },
-                {
-                    "Henry","ford",2
-                }
-        };
-    }
 
     //@Test(dataProvider = "DataForPost")
     public void test_post(String firstName, String lastName, int subjectId){
@@ -53,13 +33,8 @@ public class DataDrivenExamples1 {
             log().all();
     }
 
-    @DataProvider(name = "DeleteData")
-    public Object[] dataForDelete(){
-        return new Object[] {
-                4,5,6,7
-        };
-    }
-    @Test(dataProvider = "DeleteData")
+
+    //@Test(dataProvider = "DeleteData")
     public void test_delete(int userId){
         baseURI = "http://localhost:3000/";
 
@@ -67,5 +42,18 @@ public class DataDrivenExamples1 {
                 delete("/users/" + userId).
                 then().
                 statusCode(200);
+    }
+
+    //Case for using testNG.xml file
+    @Parameters({"userId"})
+    @Test
+    public void test_delete2(int userId){
+        System.out.println("Value for userId is: " + userId);
+        baseURI = "http://localhost:3000/";
+
+        when().
+            delete("/users/" + userId).
+        then().
+            statusCode(200);
     }
 }
